af6fe0438114f631aad22549b892da48fc453531
rickud <riccardo.ziraldo@utdallas.edu>
2018-08-30 15:08:30 -0500

    add sample image
    non saturated

--- src/main/java/gellanesfit/GelLanesFit.java
+++ src/main/java/gellanesfit/GelLanesFit.java
@@ -153,12 +153,12 @@ public class GelLanesFit implements Command {
 		final String sep = File.separator;
 		final String folder = "src" + sep + "main" + sep + "resources" + sep +
 			"sample" + sep + "Tagment-Test3" + sep
-//			+ "Gel Camera" + sep;
+			+ "Gel Camera" + sep;
 //			+ "Massa's Phone" + sep;
 //			+ "Rick's Phone" + sep;
 //			+ "Other Camera" + sep;
 //			+ "ingelico" + sep;
-		+ "tapestation" + sep;
+//		+ "tapestation" + sep;
 		// List of files available for debugging purposes
 //		String file = "second_destain.tif";
 //		String file = "LM_1_top.tif";
@@ -177,7 +177,7 @@ public class GelLanesFit implements Command {
 //		String file = "HM_1_3.tif";
 //		String file = "HM_5.tif";
 
-//		String file = "Long_1s.tif";
+		String file = "Long_1sA.tif";
 //		String file = "Long_5s.tif";
 
 //		Massa's phone
@@ -214,7 +214,7 @@ public class GelLanesFit implements Command {
 //		String file = "2igelico_090418.tif";
 //		String file = "2igelico_0904183.tif";
 //		String file = "2018_4_18 old and new p428 2nd pic.tif";
-	String file = "Shimichi_042117_4.tif";
+//	String file = "Shimichi_042117_4.tif";
 		
 		final ImagePlus iPlus = new Opener().openImage(folder + file);
 		if (iPlus.getType() == ImagePlus.GRAY8 || iPlus.getType() == ImagePlus.GRAY16) {
